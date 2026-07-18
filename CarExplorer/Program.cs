40ac26519db71f30398265761381f456e23940e4
ahmad odeh <Ahmad.raed.odeh@gmail.com>
2026-07-18 13:42:33 +0300

    Add global exception handling middleware with Serilog logging

--- CarExplorer/Program.cs
+++ CarExplorer/Program.cs
@@ -1,20 +1,22 @@
 using CarExplorer.Extensions;
+using CarExplorer.Middleware;
+using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddApplicationServices();
+
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 app.UseRouting();
 
